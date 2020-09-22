2e3e3b526193479e094e68d86adede37b837f944
Louis van der Valk <louisvandervalk@MacBook-Pro-van-Louis-van-der-Valk.local>
2020-09-22 19:12:35 +0200

    Alle bestanden uploaden.

---
+++ General-navigatie.js
@@ -0,0 +1,9 @@
+document.write('\
+<table> \
+<tr class="general-navi">                            \
+    <td class="general-navi"><a href="index.html">Facebook </a></td class="general-navi">    \
+    <td class="general-navi"><a href="over.html">LinkedIn </a></td class="general-navi">        \
+    <td class="general-navi"><a href="consult.html">Mail </a></td class="general-navi">      \
+</tr class="general-navi">   \
+</table> \
+');
